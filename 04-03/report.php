98bf6edd43506cb36a38338bf6280622d6a9897e
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-03-04 18:23:54 +0100

    report-concluso_esercizi

--- 04-03/report.php
+++ 04-03/report.php
@@ -12,33 +12,44 @@ echo "<h3> Report </h3>";
 /* -------------------------------------------------------- */
 /* report($utenti); */
 /* Conto degli utenti */
-$numeroUtenti=count($utenti);
-echo "Numero Totale di utenti: ".$numeroUtenti. "<br/>"; 
+echo "<h3>Numero Utenti</h3>";
+$numeroUtenti = count($utenti);
+echo "Numero Totale di utenti: " . $numeroUtenti . "<br/>";
 /* -------------------------------------------------------- */
 /*Media del età */
+echo "<h3>Media delle eta generale</h3>";
 /* echo "La Media del età: ". array_sum($utenti[$eta]) . "<br/>";  che non funziona perche ovviamente lui cazzo ne sa di dove la collona*/
-$eta = array_column($utenti, 'eta');// percio prendo la collona 
+$eta = array_column($utenti, 'eta'); // percio prendo la collona 
 $sommaEta = array_sum($eta); //uso la somma
 $media = $sommaEta / $numeroUtenti; //e faccio la media con la variabile che ho gia
-echo "La Media del età: ". $media . "<br/>";
+echo "La Media del età: " . $media . "<br/>";
 /* -------------------------------------------------------- */
 /*Utenti maggioreni  */
-function utentiMaggioreni($utenti) {
+echo "<h3>Utenti Maggiorenni</h3>";
+function utentiMaggioreni($utenti)
+{
     $count = 0;
     foreach ($utenti as $utente) {
-        if ($utente["eta"] >= 18) { 
+        if ($utente["eta"] >= 18) {
             $count++;
         }
     }
     echo "Numero di utenti maggiorenni: " . $count . "<br/>";
 }
-utentiMaggioreni($utenti,$eta);
+utentiMaggioreni($utenti, $eta);
 /* -------------------------------------------------------- */
 /*Utenti divisi per citta */
-/* $citta = array_column($utenti, 'citta'); */
-$utentiPerCitta=[];
-/*      foreach ($utenti as $utente) {
-        if ($utente["citta"]) { 
-            echo "neko <br/>";
-        }
-    } */
+echo "<h3>Utenti divisi per città</h3>";
+
+$utentiPerCitta = [];
+foreach ($utenti as $utente) {
+    /* $new[$child['parent']]['child'][] = $child; */
+    $utentiPerCitta[$utente['citta']][] = $utente;
+}
+
+foreach ($utentiPerCitta as $citta => $listaUtenti) {
+    echo "<h4>$citta</h4>";
+    foreach ($listaUtenti as $utente) {
+        echo $utente['nome'] . " <br>";
+    }
+}
