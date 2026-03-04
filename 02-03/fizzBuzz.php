1ef4007d4b47829db11791bf390e34dadb99851f
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-03-04 12:08:38 +0100

    migliorameti&nuova-consegna

--- 02-03/fizzBuzz.php
+++ 02-03/fizzBuzz.php
@@ -1,4 +1,8 @@
 <?php
+/* 
+Approcio iniziale:
+Funzione che Stampa da un numero minimo 1 al numero passo come parametro andando a farlo controllare se appartine a uno di 3 casi "FizzBuzz" , "Fizz" o "Buzz" altriemeti torna il numero
+
 function fizzBuzz($num)
 {
    for ($i = 1; $i <= $num; $i++) {
@@ -13,4 +17,26 @@ function fizzBuzz($num)
       }
    }
 };
-echo fizzBuzz(18);
+echo fizzBuzz(18); */
+
+/* Evoluzione:
+per addattarlo hai metodi di programmazione ideale e addato al essere utilizzato da api o richieste fututre senza la neccesaria modifica della funzione fizzBuzz ma tramite la relaizazione di altre funzioni specifiche rendendo ogni entità fine a un job specifico */
+$limit = 12;
+$result = [];
+
+function fizzBuzz(int $num): string 
+{
+   if ($num % 3 == 0 && $num % 5 == 0) {
+      return "FizzBuzz";
+   } else if ($num % 3 == 0) {
+      return "Fizz";
+   } else if ($num % 5 == 0) {
+      return "Buzz";
+   } else {
+      return "$num";
+   }
+};
+
+for ($i = 1; $i <= $limit; $i++) {
+   $result[] = fizzBuzz($i);
+}
