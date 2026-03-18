72ff449a962e72dbf88dc5859bd955daec1d9968
Brianna Ciasnog <ciasnogb1084@gmail.com>
2026-03-18 10:57:20 +0100

    improvement-report

--- 18-03/report/report.php
+++ 18-03/report/report.php
@@ -1,12 +1,59 @@
 <?php
-
 require_once "utenti.php";
 
 echo "<h3> Report </h3>";
+
 /* -------------------------------------------------------- */
-/* function report($utenti) {
+/* secondo la logica di fizzbuzz prima ho bisogno di una funzione che vada solo a prendere gli utenti */
+function countUsers(array $utenti): int
+{
+    return count($utenti);
+}
+/* se e necessario stamparli  basta prendere la funzione*/
+echo "<h3>Numero Utenti</h3>";
+echo countUsers($utenti);
+
+/* -------------------------------------------------------- */
+/*Media del età */
+function averageAge(array $utenti): float
+{
+    $eta = array_column($utenti, 'eta');
+    $somma = array_sum($eta);
+    return $somma / countUsers($utenti);
+}
+/* se e necessario stamparli  basta prendere la funzione*/
+echo "<h3>Media età</h3>";
+echo averageAge($utenti);
+/* -------------------------------------------------------- */
+/*Utenti maggioreni  */
+function countAdults(array $utenti): int
+{
+    $count = 0;
     foreach ($utenti as $utente) {
-        echo $utente["nome"] . "<br>";
+        if ($utente["eta"] >= 18) {
+            $count++;
+        }
     }
-} */
+    return $count;
+}
+/* se e necessario stamparli  basta prendere la funzione*/
+echo "<h3>Maggiorenni</h3>";
+echo countAdults($utenti);
 /* -------------------------------------------------------- */
+/*Utenti divisi per citta */
+function groupByCity(array $utenti): array
+{
+    $result = [];
+    foreach ($utenti as $utente) {
+        $result[$utente['citta']][] = $utente;
+    }
+    return $result;
+}
+$citta = groupByCity($utenti);
+/* se e necessario stamparli  basta prendere la funzione*/
+foreach ($citta as $nomeCitta => $lista) {
+    echo "<h4>$nomeCitta</h4>";
+    foreach ($lista as $utente) {
+        echo $utente['nome'] . " - ";
+    }
+}
